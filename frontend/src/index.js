1150af707d29503e705434f4bf7fc78c01483e8e
cooldude-69 <yash.srs55@gmail.com>
2025-08-14 18:35:16 +0530

    Fix Redux store import - use named import instead of default import

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { Toaster } from "react-hot-toast";
-import store from "./store";
+import { store } from "./store";
 import App from "./App";
 import "./index.css";
 
